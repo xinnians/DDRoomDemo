999b772105f42ae7ff5393fe58455db700b720e5
Ian Lin <ian.lin@yjtech.tw>
2020-09-11 19:21:08 +0800

    [modify] 設置timer3秒，時間到後執行開啟瀏覽器動作，並設置當app被意外關閉時隨即停止timer及後續動作

--- app/src/main/java/com/example/ddroom/MainActivity.kt
+++ app/src/main/java/com/example/ddroom/MainActivity.kt
@@ -3,10 +3,19 @@ package com.example.ddroom
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+
+    //欲連結網址
+    private val mUri = Uri.parse("https://www.dd-room.com/")
+    //欲停留時間
+    private val mWaitTime = 3000L
+    //timer object
+    private var mTimer: CountDownTimer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,9 +23,35 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        startTimer(mWaitTime)
+    }
 
-        //以外開瀏覽器的方式開啟連結"https://www.dd-room.com/"
-        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.dd-room.com/"))
+    override fun onPause() {
+        super.onPause()
+        stopTimer()
+    }
+
+    //啟動timer
+    private fun startTimer(waitTime: Long) {
+        mTimer = object : CountDownTimer(waitTime, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+            }
+
+            override fun onFinish() {
+                openBrowser(mUri)
+            }
+        }
+        (mTimer as CountDownTimer).start()
+    }
+
+    //停止timer
+    private fun stopTimer() {
+        mTimer?.cancel()
+    }
+
+    //以外開瀏覽器的方式開啟連結uri
+    private fun openBrowser(uri: Uri) {
+        val browserIntent = Intent(Intent.ACTION_VIEW, uri)
         startActivity(browserIntent)
     }
 }
